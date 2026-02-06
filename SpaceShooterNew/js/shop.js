e863b62bd8d002e261f8adc05c54e59e6dca04df
ixviPCs <ixvipcs@gmail.com>
2026-02-06 13:08:56 -0800

    Update shop.js

--- SpaceShooterNew/js/shop.js
+++ SpaceShooterNew/js/shop.js
@@ -1,5 +1,5 @@
 // shop.js
-const shopItemsData = [{ name: "Ally", sprite: "<img src='ally.png'>", action: buyAlly }];
+const shopItemsData = [{ name: "Ally", sprite: "<img src='../Images/ally.png'>", action: buyAlly }];
 
 function openShop(){ shopOpen=true; codeBox.style.display="none"; document.getElementById("shop").style.display="block"; document.getElementById("coinDisplay").innerText=coins; pauseGame(); renderShop(); }
 function closeShop(){ shopOpen=false; document.getElementById("shop").style.display="none"; resumeGame(); }
@@ -10,7 +10,7 @@ function renderShop(){
   const cost = allyCount < 4 ? allyCostLadder[allyCount] : "MAX";
   const div = document.createElement("div");
   div.style.display="flex"; div.style.justifyContent="space-between"; div.style.background="#222"; div.style.padding="8px"; div.style.borderRadius="6px";
-  div.innerHTML = `<span><img src="ally.png" style="width:16px;height:16px;"> Ally - ${cost}💰</span><button>${cost==="MAX"?"MAX":"Buy"}</button>`;
+  div.innerHTML = `<span><img src="../Images/ally.png" style="width:16px;height:16px;"> Ally - ${cost}💰</span><button>${cost==="MAX"?"MAX":"Buy"}</button>`;
   const btn = div.querySelector("button");
   if(cost !== "MAX") btn.onclick = buyAlly; else btn.disabled = true;
   container.appendChild(div);
